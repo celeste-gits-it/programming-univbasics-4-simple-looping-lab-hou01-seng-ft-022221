ddf404f9e5169121ccb53add34db866e24970f78
Celeste Carter <celesteanell@gmail.com>
2021-02-08 20:14:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,9 @@
 # Write your methods here 
 
 def loop_message_five_times(message)
-  message = "Good Vibes"
   counter = 0
-  
+    message = "Good Vibes"
+
   while counter < 5 do
     puts message
       counter += l
