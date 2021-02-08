1aacd03d41852af64802c2d51d9b030aaa596abe
Celeste Carter <celesteanell@gmail.com>
2021-02-08 20:33:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,21 +21,3 @@ counter = 0
   end
 end
 
-def output_array(array)
-counter = 0
-
-  while array[counter] do
-  puts array[counter]
-  counter += 1 
-  end
-end
-
-def return_string_array(array)
-  count = 0
- 
-  while count < array.length do
-  array[count] = array[count].to_s
-  count += 1
-  end
-  array
-end
