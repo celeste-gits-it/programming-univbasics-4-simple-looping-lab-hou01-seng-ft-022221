66614069bb3b2b816b7a3e5247842043dbe3a79d
Celeste Carter <celesteanell@gmail.com>
2021-02-08 20:26:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,12 +1,11 @@
 # Write your methods here 
 
 def loop_message_five_times(message)
-  message = "good morning"
-  counter = 0
+message = "Hello World."
+counter = 0
 
-  while counter < 5 do
-    puts message
-    counter += l
-    end
+  while counter < 5 do 
+  puts message
+  counter += 1
   end
-    
+end
